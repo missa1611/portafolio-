3e9bee58c29d243dc6fe6a6aec611ae239044d6b
Misael <misasantose301@gmail.com>
2026-04-23 19:47:44 +1200

    Mi portafolio web

---
+++ script.js
@@ -0,0 +1,47 @@
+// menú hamburguesa
+function toggleMenu(){
+const menu = document.getElementById("menu");
+if(menu){
+menu.classList.toggle("activo");
+}
+}
+
+// animación scroll
+const elementos = document.querySelectorAll(".presentacion, .frase, .perfil");
+
+//  agregado
+const nuevosElementos = document.querySelectorAll(".formacion, .proyectos");
+
+// 🔽 nuevo (para imagen bienvenida, sin repetir variable)
+const imagenBienvenida = document.querySelectorAll(".bienvenida-img");
+
+const mostrarScroll = () => {
+const trigger = window.innerHeight * 0.85;
+
+elementos.forEach(el => {
+const top = el.getBoundingClientRect().top;
+if(top < trigger){
+el.classList.add("visible");
+}
+});
+
+// agregado
+nuevosElementos.forEach(el => {
+const top = el.getBoundingClientRect().top;
+if(top < trigger){
+el.classList.add("visible");
+}
+});
+
+// 🔽 agregado para imagen
+imagenBienvenida.forEach(el => {
+const top = el.getBoundingClientRect().top;
+if(top < trigger){
+el.classList.add("visible");
+}
+});
+
+};
+
+window.addEventListener("scroll", mostrarScroll);
+window.addEventListener("load", mostrarScroll);
